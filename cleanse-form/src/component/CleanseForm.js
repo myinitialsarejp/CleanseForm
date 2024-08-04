662090195076716d91f35ee39df770591a4d12dc
Jesse Pelletier <jespel2013@gmail.com>
2024-08-04 16:24:49 -0400

    skelaton

---
+++ cleanse-form/src/component/CleanseForm.js
@@ -0,0 +1,15 @@
+import { Dialog } from '@mui/material'
+import * as React from 'react'
+
+const CleanseForm = (props) => {
+
+
+
+    return (
+        <Dialog>
+            Dialog
+        </Dialog>
+    )
+}
+
+export default CleanseForm
